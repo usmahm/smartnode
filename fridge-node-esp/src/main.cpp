532e5a0a259a898cd43b723adc83f722de25b21d
Ahmad Usman <ahmadujuwon@gmail.com>
2024-04-29 23:16:16 +0100

    feat: change board to arduino uno

--- fridge-node-esp/src/main.cpp
+++ fridge-node-esp/src/main.cpp
@@ -3,7 +3,7 @@
 #include <ThreeWire.h>  
 #include <RtcDS1302.h>
 
-ThreeWire myWire(D4,D5,D2); // IO, SCLK, CE
+ThreeWire myWire(4, 5, 2); // IO, SCLK, CE
 RtcDS1302<ThreeWire> Rtc(myWire);
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
@@ -24,14 +24,14 @@ void printDateTime(const RtcDateTime& dt)
     Serial.print(datestring);
 }
 
-const int relay = 5;
+const int relay = 13;
 unsigned long on_millis;
 bool is_on = false;
 
-const uint8_t on_hour = 4;
-const uint8_t on_minute = 40;
+uint8_t on_hour = 8;
+uint8_t on_minute = 55;
 
-int on_duration = 1 * 60 * 1000;
+unsigned long on_duration = 60000;
 
 
 void setup() {
@@ -46,6 +46,43 @@ void setup() {
   RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
   printDateTime(compiled);
   Serial.println();
+
+  if (!Rtc.IsDateTimeValid()) 
+    {
+        // Common Causes:
+        //    1) first time you ran and the device wasn't running yet
+        //    2) the battery on the device is low or even missing
+
+        Serial.println("RTC lost confidence in the DateTime!");
+        Rtc.SetDateTime(compiled);
+    }
+
+    if (Rtc.GetIsWriteProtected())
+    {
+        Serial.println("RTC was write protected, enabling writing now");
+        Rtc.SetIsWriteProtected(false);
+    }
+
+    if (!Rtc.GetIsRunning())
+    {
+        Serial.println("RTC was not actively running, starting now");
+        Rtc.SetIsRunning(true);
+    }
+
+    RtcDateTime now = Rtc.GetDateTime();
+    if (now < compiled) 
+    {
+        Serial.println("RTC is older than compile time!  (Updating DateTime)");
+        Rtc.SetDateTime(compiled);
+    }
+    else if (now > compiled) 
+    {
+        Serial.println("RTC is newer than compile time. (this is expected)");
+    }
+    else if (now == compiled) 
+    {
+        Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+    }
 }
 
 void loop() {
@@ -57,8 +94,8 @@ void loop() {
   Serial.println(millis());
   Serial.println("---------");
   
-  if (datetime.Hour() == 0 && datetime.Minute() == 30 && !is_on) {
-    digitalWrite(relay, LOW);
+  if (datetime.Hour() == on_hour && datetime.Minute() == on_minute && !is_on) {
+    digitalWrite(relay, HIGH);
     
     Serial.println(datetime.Hour());
     Serial.println(datetime.Minute());
@@ -70,9 +107,14 @@ void loop() {
   }
 
   if (is_on && (millis() - on_millis >= on_duration)) {
-    digitalWrite(relay, HIGH);
+    digitalWrite(relay, LOW);
     Serial.println("Current turned off");
     is_on = false;
+
+    on_hour = 10;
+    on_minute = 0;
+
+    on_duration = 14400000;
   }
   
   delay(5000);
