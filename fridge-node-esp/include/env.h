173aa0e6021053a2e941fbfc2b154730f78c782f
Ahmad Usman <ahmadujuwon@gmail.com>
2024-07-15 11:11:39 +0100

    feat: working on esp01 module

--- fridge-node-esp/include/env.h
+++ fridge-node-esp/include/env.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define WIFISSID "raspit"
-#define WIFI_PASSWORD "raspit1ras"
+// #define WIFISSID "raspit"
+// #define WIFI_PASSWORD "raspit1ras"
 
 // #define RELAY_PIN D5
 #define RELAY_PIN 2
@@ -10,5 +10,5 @@
 #define API_BASE_URL "http://192.168.0.162:3001"
 #define UTC_OFFSET 1 // Timezone UTC offset
 
-// #define WIFISSID "RAIN 23 CLASS 2G"
-// #define WIFI_PASSWORD "Rainigeria."
+#define WIFISSID "STARLINK"
+#define WIFI_PASSWORD "Rainigeria."
--- fridge-node-esp/src/main.cpp
+++ fridge-node-esp/src/main.cpp
@@ -1,17 +1,16 @@
 #include <Arduino.h>
-// #include "customLogger.h"
-// #include "webServer.h"
+#include "customLogger.h"
+#include "webServer.h"
 #include "wifi.h"
-// #include "httpFunctions.h"
-// #include "timeFunctions.h"
+#include "httpFunctions.h"
+#include "timeFunctions.h"
 
 // Relay
 unsigned long on_millis;
-// unsigned long on_duration = 30000;
 unsigned long on_duration = 18000000;
 bool is_on = false;
-const uint8_t on_hour = 10;
-const uint8_t on_minute = 1;
+const uint8_t on_hour = 8;
+const uint8_t on_minute = 3;
 
 void setup() {
   Serial.begin(115200);
@@ -31,36 +30,36 @@ void loop() {
   CustomLogger::println(millis());
   CustomLogger::println("---------");
   
-  // int hour = timeClient.getHours();
-  // int minute = timeClient.getMinutes();
+  int hour = timeClient.getHours();
+  int minute = timeClient.getMinutes();
 
-  // String formatted = timeClient.getFormattedTime();
+  String formatted = timeClient.getFormattedTime();
 
-  // CustomLogger::println(hour);
-  // CustomLogger::println(minute);
-  // CustomLogger::print(formatted);
-  // // CustomLogger::print(minute);
+  CustomLogger::println(hour);
+  CustomLogger::println(minute);
+  CustomLogger::print(formatted);
+  // CustomLogger::print(minute);
 
-  // if (hour == on_hour && minute == on_minute && !is_on) {
-  //   digitalWrite(RELAY_PIN, HIGH);
+  if (hour == on_hour && minute == on_minute && !is_on) {
+    digitalWrite(RELAY_PIN, HIGH);
     
-  //   on_millis = millis();
-  //   is_on = true;
+    on_millis = millis();
+    is_on = true;
 
-  //   CustomLogger::println(hour);
-  //   CustomLogger::println(minute);
-  //   CustomLogger::print("Current Flowing for: ");
-  //   CustomLogger::println(on_duration);
+    CustomLogger::println(hour);
+    CustomLogger::println(minute);
+    CustomLogger::print("Current Flowing for: ");
+    CustomLogger::println(on_duration);
 
-  //   postRelayStatus("on");
-  // }
+    postRelayStatus("on");
+  }
 
-  // if (is_on && (millis() - on_millis >= on_duration)) {
-  //   digitalWrite(RELAY_PIN, LOW);
-  //   CustomLogger::println("Current turned off");
-  //   is_on = false;
-  //   postRelayStatus("off");
-  // }
+  if (is_on && (millis() - on_millis >= on_duration)) {
+    digitalWrite(RELAY_PIN, LOW);
+    CustomLogger::println("Current turned off");
+    is_on = false;
+    postRelayStatus("off");
+  }
   
   delay(5000);
 }
